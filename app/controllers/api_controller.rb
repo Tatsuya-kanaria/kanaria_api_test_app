9636cd05a42962d412250d0619f4acfe58c4100d
Tatsuya-kanaria <kanaria.git@gmail.com>
2021-03-08 18:58:07 +0900

    テスト

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
 
   # レスポンス用
   def success
-    # rails s -p 3000 -> http://localhost:3000/api 
+    # rails s -p 3000 -> http://localhost:3000/api
     render status: 200, json: { status: 200, message: "Success" }
   end
 end
